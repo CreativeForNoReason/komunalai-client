7ed5891d2d6d087c7308449b6a2d8387b092bdb9
CreativeForNoReason <domasnorkunas@gmail.com>
2023-05-08 00:13:56 +0300

    container added for sizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // App.tsx
 import React from 'react';
 import FormContainer from './Components/FormContainer';
+import Container from 'react-bootstrap/esm/Container';
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <h1>My Form</h1>
       <FormContainer />
-    </div>
+    </Container>
   );
 };
 
--- src/Components/ComuteForm.tsx
+++ src/Components/ComuteForm.tsx
@@ -4,6 +4,7 @@ import { FormDataContext } from './FormContainer';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/esm/Form';
+import Container from 'react-bootstrap/esm/Container';
 
 interface ComuteFormProps {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
@@ -13,41 +14,43 @@ const ComuteForm: React.FC<ComuteFormProps> = ({ onSubmit }) => {
   const { formData, handleChange } = useContext(FormDataContext);
 
   return (
-    <Form onSubmit={onSubmit}>
-      <Form.Group className="mb-3" controlId="firstName">
-        <Form.Label>First Name</Form.Label>
-        <Form.Control
-          type="text"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleChange}
-        />
-      </Form.Group>
+    <Container>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="firstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="lastName">
-        <Form.Label>Last Name</Form.Label>
-        <Form.Control
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-        />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="lastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-      <Button variant="outline-primary" type="submit">
-        Save
-      </Button>
-    </Form>
+        <Button variant="outline-primary" type="submit">
+          Save
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
